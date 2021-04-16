b1c2f12f241b54109364b4e2e55d5de7a75f789c
Gary <tharun@squareshift.co>
2021-04-16 18:59:48 +0530

    Staging environment of Kohls

--- script.js
+++ script.js
@@ -260,5 +260,29 @@ window.onload = function () {
       }
     };
     let modifyContentInterval = setInterval(modifyContent, 2000);
+  } else if (path.includes("kohls")) {
+    console.log("It is kohls");
+    // Create tag at right corner
+    let body = document.querySelector("body");
+    let tag = document.createElement("div");
+    tag.innerText = "STAGING";
+    tag.className += "corner-ribbon";
+    tag.style.cssText =
+      "width: 200px; background: #2c7; z-index: 1000; box-shadow: 0 0 3px rgb(0 0 0 / 30%); position: fixed; font-size: 20px;  text-align: center; line-height: 50px; letter-spacing: 1px; color: #fff;  top: auto; bottom: 25px; left: -50px; transform: rotate(45deg); -webkit-transform: rotate(45deg);";
+    body.appendChild(tag);
+
+    // Create style document
+    let styles =
+      ".top-global-header #navigation.dropdown-navigation{background: transparent;} .top-global-header .dropdown-navigation .navigation-item{background: #11518f;} .top-global-header .dropdown-navigation .navigation-item:hover, .top-global-header .dropdown-navigation-container .nav-overlay{background: #2c76bd !important;} .top-global-header #navigation li h2 a, .nav-overlay h3 a, .nav-overlay a, .top-global-header .dropdown-navigation .nav-overlay h3 {color: #fff !important} .nav-overlay h3 a font{ color: #f27521 !important;} .top-global-header #new-equity-banner {background: #f27521; border-botton: none;} .top-global-header #new-equity-banner a, .top-global-header #tr_phase2_ShoppingBg .close-bag .kas-newpb-shopping-bag-label {color: #fff !important;} .top-global-header #header-container {background: #115090;} #open-drawer.closed, #open-drawer.closed .semi-circle, #open-drawer.open button.semi-circle, #open-drawer p.offer-ticker { background: #f27521 !important;} #kohls_logo svg g path {fill: #fff;} #utility-nav svg g path, #checkout-container svg g path{ fill: #fff;} .top-global-header .greeting-container, .top-global-header .account-signin, .top-global-header #utility-nav a, .top-global-header #checkout-container .checkout-link, .top-global-header .mini-cart-header .number-items, .top-global-header .mini-cart-header .subtotal {color: #fff;} .top-global-header #utility-nav:hover, .top-global-header .skava-mini-cart:hover, .top-global-header #checkout-container:hover, .utility-nav-background-color {background-color: #f27521 !important} .top-global-header .dropdownsignout {background-color: #134f8ebf;} .top-global-header .dropdownsignout .dropdownsigninbtn #pb_signin {background-color: #f27521} .top-global-header .dropdownsignout ul li {background-color: #fff; } .top-global-header #utility-nav a:hover, .top-global-header #utility-nav-wallet .utility-nav-wallet-text a:hover, .top-global-header #utility-nav .create-account:hover, .top-global-header #utility-nav a.create-account:hover { color: #fff !important; } #utility-nav-wallet .utility-nav-wallet-text a, .top-global-header .yes2you-logo-text a, .top-global-header .yes2you-logo-text:hover a, .top-global-header .kohls-cash-logo-text a, .top-global-header .kohls-cash-logo-text:hover a { color: #f27521 !important;} .tr_phase2_purchase_earning_meter, .tr_phase2_order_summary_ghr h5, .tr_phase2_order_summary_ghr h6, .tr_phase2_order_summary_ghr h4, .tr_phase2_order_summary_right-float {color: #fff;} .tr_phase2_headerPanel, .tr_phase2_headerPanel {background: #134f8ebf !important;} .tr_phase2_sub_header, .tr_phase2_shopping-bag_content { background: transparent;} .mini-cart-header.loadedNewPB { background-color: #f27521 !important;} .tr_phase2_purchase_earning_meter_signup a, .tr_phase2_purchase_earning_meter_signup a:hover  {color: #fff;} .hp2-creative .moms-bubbles__nav .divider, .creative-slot.creative-slot.hero .hero__categories {background: #11518f !important;} .hp2-creative .moms-bubbles__nav a.bubble-link, .creative-slot.creative-slot.hero .hero__flex .hero__flex__box {border: 5px solid #11518fa6 !important;} .hp2-creative .moms-bubbles__nav .bubble-link p, .creative-slot.creative-slot.hero .hero__flex .hero__flex__top, .creative-slot.creative-slot.hero .hero__flex .hero__flex__cta, .creative-slot.creative-slot.hero .hero__flex .hero__flex__price {color: #f27621 !important;} .hp2-creative #hp2__bkg__block {background-color: #f2752199 !important;} .footer-global {background-color: #134f8ebf !important;} .footer-slot .footer-links__category-list__link, .footer-slot .footer-top__header--social, .footer-slot .footer-top__header--native-app, #footer .footer-legal__copy p, .footer-legal__copy p, .footer-slot .footer-top__native-app__badges p, .footer-top__native-app__badges p, .footer-slot .footer-legal a {color: #fff !important;} .footer-slot .footer-top__native-app .scancode__button {background-color: #f27521 !important; text-decoration: none !important;} .footer-top__social-list__link svg path{fill: #fff !important;} .footer-slot .footer-links__category__header {color: #f27521 !important;}";
+    let css = document.createElement("style");
+    css.type = "text/css";
+    if (css.styleSheet) css.styleSheet.cssText = styles;
+    else css.appendChild(document.createTextNode(styles));
+    // Append style to the tag name
+    document.getElementsByTagName("head")[0].appendChild(css);
+
+    Array.from(document.querySelectorAll("*[class]")).forEach((item) => {
+      item.className = item.className + " " + item.classList[0] + "-proxy";
+    });
   }
 };
